6f8046c6aa78fff81ac274cd61dd40f914989506
zcj <18137693952@163.com>
2022-07-18 14:47:15 +0800

    init: cli

---
+++ packages/cli/commands/build.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+const { defineConfig, build } = require('vite');
+const vue = require('@vitejs/plugin-vue');
+const vueJsx = require('@vitejs/plugin-vue-jsx');
+const nuxtBuild = require('./build-nuxt-auto-import');
+const { isReadyToRelease } = require('../shared/utils');
+
+const entryDir = path.resolve(__dirname, '../../ccui');
+const outputDir = path.resolve(__dirname, '../../build');
+
+const baseConfig = defineConfig({
+  configFile: false,
+  publicDir: false,
+  plugins: [vue(), vueJsx()]
+});
+
+const rollupOptions = {
+  external: ['vue', 'vue-router', '@vueuse/core', '@floating-ui/dom'],
+  output: {
+    globals: {
+      vue: 'Vue'
+    }
+  }
+};
+
+const buildSingle = async (name) => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        rollupOptions,
+        lib: {
+          entry: path.resolve(entryDir, name),
+          name: 'index',
+          fileName: 'index',
+          formats: ['es', 'umd']
+        },
+        outDir: path.resolve(outputDir, name)
+      }
+    })
+  );
+};
+
+const buildAll = async () => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        rollupOptions,
+        lib: {
+          entry: path.resolve(entryDir, 'vue-devui.ts'),
+          name: 'VueDevui',
+          fileName: 'vue-devui',
+          formats: ['es', 'umd']
+        },
+        outDir: outputDir
+      }
+    })
+  );
+};
+
+const createPackageJson = (name) => {
+  const fileStr = `{
+  "name": "${name}",
+  "version": "0.0.0",
+  "main": "index.umd.js",
+  "module": "index.es.js",
+  "style": "style.css"
+}`;
+
+  fsExtra.outputFile(
+    path.resolve(outputDir, `${name}/package.json`),
+    fileStr,
+    'utf-8'
+  );
+};
+
+exports.build = async () => {
+  await buildAll();
+
+  const components = fs.readdirSync(entryDir).filter((name) => {
+    const componentDir = path.resolve(entryDir, name);
+    const isDir = fs.lstatSync(componentDir).isDirectory();
+    return isDir && fs.readdirSync(componentDir).includes('index.ts');
+  });
+
+  for (const name of components) {
+    if (!isReadyToRelease(name)) {
+      continue;
+    }
+    await buildSingle(name);
+    createPackageJson(name);
+    nuxtBuild.createAutoImportedComponent(name);
+  }
+
+  nuxtBuild.createNuxtPlugin();
+};
---
+++ packages/cli/shared/constant.js
@@ -0,0 +1,92 @@
+const { resolve } = require('path');
+const { version } = require('../package.json');
+
+exports.VERSION = version;
+exports.isProd = process.env.NODE_ENV === 'production';
+exports.CWD = process.cwd();
+exports.DEVUI_DIR = resolve(this.CWD, '../okUiVue/okUi');
+exports.DEVUI_NAMESPACE = 'K';
+exports.CSS_CLASS_PREFIX = 'okUi';
+exports.TESTS_DIR_NAME = '__tests__';
+exports.INDEX_FILE_NAME = 'index.ts';
+exports.DOCS_FILE_NAME = 'index.md';
+exports.VUE_DEVUI_IGNORE_DIRS = ['shared', 'style'];
+exports.VUE_DEVUI_FILE_NAME = 'okUiVue.ts';
+exports.VUE_DEVUI_FILE = resolve(this.DEVUI_DIR, this.VUE_DEVUI_FILE_NAME);
+exports.SITES_DIR = resolve(this.CWD, '../okUiVue/docs');
+exports.SITES_COMPONENTS_DIR_NAME = 'components';
+exports.SITES_COMPONENTS_DIR_NAME_EN = 'en-US/components';
+exports.SITES_COMPONENTS_DIR = resolve(
+  this.SITES_DIR,
+  this.SITES_COMPONENTS_DIR_NAME
+);
+exports.VITEPRESS_DIR = resolve(this.SITES_DIR, '.vitepress');
+exports.VITEPRESS_SIDEBAR_FILE_NAME = 'sidebar.ts';
+exports.VITEPRESS_SIDEBAR_FILE = resolve(
+  this.VITEPRESS_DIR,
+  `config/${this.VITEPRESS_SIDEBAR_FILE_NAME}`
+);
+exports.VITEPRESS_SIDEBAR_FILE_NAME_EN = 'enSidebar.ts';
+exports.VITEPRESS_SIDEBAR_FILE_EN = resolve(
+  this.VITEPRESS_DIR,
+  `config/${this.VITEPRESS_SIDEBAR_FILE_NAME_EN}`
+);
+// 这里的分类顺序将会影响最终生成的页面侧边栏顺序
+exports.VITEPRESS_SIDEBAR_CATEGORY = [
+  '通用',
+  '导航',
+  '反馈',
+  '数据录入',
+  '数据展示',
+  '布局'
+];
+exports.VITEPRESS_SIDEBAR_CATEGORY_EN = [
+  'General',
+  'Navigation',
+  'Feedback',
+  'Data Entry',
+  'Data Display',
+  'Layout'
+];
+exports.VITEPRESS_SIDEBAR_CATEGORY_ZH_TO_EN = {
+  通用: 'General',
+  导航: 'Navigation',
+  反馈: 'Feedback',
+  数据录入: 'Data Entry',
+  数据展示: 'Data Display',
+  布局: 'Layout'
+};
+exports.COMPONENT_PARTS_MAP = [
+  {
+    name: 'component（组件）',
+    value: 'component'
+  },
+  {
+    name: 'directive（指令）',
+    value: 'directive'
+  },
+  {
+    name: 'service（服务）',
+    value: 'service'
+  }
+];
+
+exports.CREATE_SUPPORT_TYPE_MAP = Object.freeze({
+  component: 'component',
+  'vue-okUi': 'vue-okUi',
+  'theme-variable': 'theme-variable'
+});
+exports.CREATE_SUPPORT_TYPES = Object.keys(this.CREATE_SUPPORT_TYPE_MAP);
+exports.CREATE_UNFINISHED_TYPES = [];
+exports.WHITE_LIST_READY_COMPONENTS = [
+  'select',
+  'tooltip',
+  'table',
+  'tabs',
+  'form',
+  'dropdown',
+  'drawer',
+  'date-picker',
+  'input-number',
+  'tree'
+];
