7f3e950c54b44781c4b8442d6fbb88372c16bf9a
zcj <18137693952@163.com>
2022-07-18 16:25:16 +0800

    feat: 更改生成主题配置

--- packages/cli/commands/generate-theme.js
+++ packages/cli/commands/generate-theme.js
@@ -1,16 +1,23 @@
 require('esbuild-register');
 const path = require('path');
 const fs = require('fs-extra');
+const logger = require('../shared/logger');
 const theme = require('../../ccui/ui/theme/themes/light.ts').default;
+const { CSS_CLASS_PREFIX } = require('../shared/constant');
 
 const fileStr = Object.entries(theme)
-  .map(([key, value]) => `$${key}: var(--${key}, ${value})`)
+  .map(([key, value]) => `$${CSS_CLASS_PREFIX}-${key}: var(--${key}, ${value})`)
   .join(';\n');
 
 exports.generateTheme = async () => {
-  await fs.outputFile(
-    path.resolve(__dirname, '../../ccui/ui/theme/theme.scss'),
-    fileStr,
-    'utf-8'
+  const outputFilePath = path.resolve(
+    __dirname,
+    '../../ccui/ui/theme/theme.scss'
   );
+  try {
+    await fs.outputFile(outputFilePath, fileStr, 'utf-8');
+    logger.success(`生成主题文件成功!, ${outputFilePath}`);
+  } catch (err) {
+    logger.success('生成主题文件失败！');
+  }
 };
