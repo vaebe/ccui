29c97a07367df3294350958eafaef32d47f1bb7f
zcj <18137693952@163.com>
2022-07-18 15:57:20 +0800

    feat: card 增加 css namespace

--- packages/ccui/ui/card/src/card.tsx
+++ packages/ccui/ui/card/src/card.tsx
@@ -1,12 +1,18 @@
 import { defineComponent, computed } from 'vue';
 import { cardProps, CardProps } from './card-types';
 import './card.scss';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 
 export default defineComponent({
   name: 'KCard',
   props: cardProps,
   setup(props: CardProps, { slots }) {
-    const boxClass = `okUi-card okUi-card-${props.shadow}-shadow`;
+    const ns = useNamespace('card');
+
+    // ccui-card ccui-card__nse ccui-card--nsm ccui-card__em--open
+    console.log(ns.b(), ns.e('nse'), ns.m('nsm'), ns.em('em', 'open'));
+
+    const boxClass = `${ns.b()} ${ns.m(props.shadow)}-shadow`;
 
     const isHeader = computed(() => {
       return props.header || slots.header;
@@ -14,10 +20,10 @@ export default defineComponent({
 
     return () => (
       <div class={boxClass}>
-        <div class='okUi-card-header' v-show={isHeader}>
+        <div class={ns.m('header')} v-show={isHeader}>
           {(slots.header && slots.header()) || props.header}
         </div>
-        <div class='okUi-card-body' style={props.bodyStyle}>
+        <div class={ns.m('body')} style={props.bodyStyle}>
           {slots.default && slots.default()}
         </div>
       </div>
