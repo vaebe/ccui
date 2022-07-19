5677c24d1355c1e048fb9379c4dafc11795606b3
zcj <18137693952@163.com>
2022-07-19 14:48:11 +0800

    cli: 额外打包深色主题文件

--- packages/cli/commands/generate-theme.js
+++ packages/cli/commands/generate-theme.js
@@ -2,21 +2,42 @@ require('esbuild-register');
 const path = require('path');
 const fs = require('fs-extra');
 const logger = require('../shared/logger');
-const theme = require('../../ccui/ui/theme/themes/light.ts').default;
 const { CSS_CLASS_PREFIX } = require('../shared/constant');
+const theme = require('../../ccui/ui/theme/themes/light.ts').default;
+const darkTheme = require('../../ccui/ui/theme/themes/dark.ts').default;
 
-const fileStr = Object.entries(theme)
-  .map(([key, value]) => `$${CSS_CLASS_PREFIX}-${key}: var(--${key}, ${value})`)
-  .join(';\n');
+const getFileStr = (data) => {
+  return Object.entries(data)
+    .map(
+      ([key, value]) => `$${CSS_CLASS_PREFIX}-${key}: var(--${key}, ${value})`
+    )
+    .join(';\n');
+};
+
+const lightFileStr = getFileStr(theme);
+
+const darkFileStr = `.dark{
+${getFileStr(darkTheme)}
+}`;
+
+console.log(darkFileStr, 'pppp');
 
 exports.generateTheme = async () => {
-  const outputFilePath = path.resolve(
+  const lightThemeFilePath = path.resolve(
     __dirname,
     '../../ccui/ui/theme/theme.scss'
   );
+  const darkThemeFilePath = path.resolve(
+    __dirname,
+    '../../ccui/ui/theme/darkTheme.scss'
+  );
+
   try {
-    await fs.outputFile(outputFilePath, fileStr, 'utf-8');
-    logger.success(`生成主题文件成功, ${outputFilePath}!`);
+    await fs.outputFile(lightThemeFilePath, lightFileStr, 'utf-8');
+    logger.success(`生成主题文件成功, ${lightThemeFilePath}!`);
+
+    await fs.outputFile(darkThemeFilePath, darkFileStr, 'utf-8');
+    logger.success(`生成主题文件成功, ${darkThemeFilePath}!`);
   } catch (err) {
     logger.success('生成主题文件失败！');
   }
