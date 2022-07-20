4cbed04ad5454f39c3a0ff42e74bc442c208bd57
zcj <18137693952@163.com>
2022-07-20 21:53:49 +0800

    docs: 更新项目名

--- packages/ccui/docs/.vitepress/config/index.ts
+++ packages/ccui/docs/.vitepress/config/index.ts
@@ -4,8 +4,8 @@ import sidebar from './sidebar';
 
 export default {
   lang: 'en-US',
-  title: 'vue-cc-ui',
-  description: ' vue-cc-ui 组件库',
+  title: 'vue3-ccui',
+  description: 'vue3-ccui 组件库',
   lastUpdated: true,
   head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
   markdown,
