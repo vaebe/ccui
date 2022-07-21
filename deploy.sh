39fe54480bf8628eca80e6329cc563cf4bbfcb70
zcj <18137693952@163.com>
2022-07-21 14:38:43 +0800

    feat: 增加 sh 发布文档文件

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+pnpm docs:build
+
+# cd 到构建输出的目录下
+cd packages/ccui/docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/vaebe/ccui.git master:gh-pages
+
+cd -
