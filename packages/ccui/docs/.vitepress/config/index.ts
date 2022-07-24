c2daee70ed24bf2c0c6fb0bf1242b5ec0ece6112
zcj <18137693952@163.com>
2022-07-24 14:54:08 +0800

    docs: 修改 vitepress 配置 base 路径

--- packages/ccui/docs/.vitepress/config/index.ts
+++ packages/ccui/docs/.vitepress/config/index.ts
@@ -2,16 +2,19 @@ import nav from './nav';
 import markdown from './markdown';
 import sidebar from './sidebar';
 
-export default {
-  lang: 'en-US',
-  title: 'vue3-ccui',
-  description: 'vue3-ccui 组件库',
-  lastUpdated: true,
-  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
-  markdown,
-  themeConfig: {
-    sidebar,
-    nav,
-    logo: '/logo.svg'
-  }
+export default ({ mode }) => {
+  return {
+    base: mode === 'development' ? '/' : '/ccui/',
+    lang: 'en-US',
+    title: 'vue3-ccui',
+    description: 'vue3-ccui 组件库',
+    lastUpdated: true,
+    head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
+    markdown,
+    themeConfig: {
+      sidebar,
+      nav,
+      logo: '/logo.svg'
+    }
+  };
 };
