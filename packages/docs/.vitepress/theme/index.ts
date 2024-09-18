f150cf1f986e8d3a3b0902e36593f389fc1b0ba7
vaebe <18137693952@163.com>
2024-09-18 21:27:01 +0800

    chore(docs): 部署配置

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DemoPreview, { useComponents } from '@vitepress-code-preview/container'
 import DefaultTheme from 'vitepress/theme'
-import ccui from '../../../ccui/ui/vue-ccui'
+import ccui from 'vue3-ccui/ui/vue-ccui'
 import './styles/index.css'
 import '@vitepress-code-preview/container/dist/style.css'
 
