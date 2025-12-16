405cc451202dbea0b616c7f032ff18248fff3e11
vae <18137693952@163.com>
2025-12-16 15:47:32 +0800

    feat(docs): 集成 UnoCSS

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 import ccui from 'vue3-ccui/ui/vue-ccui'
 import './styles/index.css'
 import '@vitepress-code-preview/container/dist/style.css'
+import 'virtual:uno.css'
 
 export default {
   ...DefaultTheme,
--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,9 +1,10 @@
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { viteDemoPreviewPlugin } from '@vitepress-code-preview/plugin'
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
-  plugins: [viteDemoPreviewPlugin(), vueJsx(), svgLoader()],
+  plugins: [viteDemoPreviewPlugin(), vueJsx(), svgLoader(), UnoCSS()],
 })
